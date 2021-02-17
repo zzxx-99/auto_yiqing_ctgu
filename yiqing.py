350d92a2cb09d3fc1d89995f5b4c6d43cf7dc291
zzxx-99 <71274663+zzxx-99@users.noreply.github.com>
2021-02-17 21:46:06 +0800

    Update yiqing.py

--- yiqing.py
+++ yiqing.py
@@ -19,7 +19,7 @@ def report(usr,pas):
     sess.headers['Host'] = 'yiqing.ctgu.edu.cn'
     sess.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-    sess.headers['Connection'] =  'close'
+    sess.headers['Connection'] =  'keep-alive'
     sess.headers['X-Requested-With'] = 'XMLHttpRequest'
     sess.headers['Upgrade-Insecure-Requests'] = '1'
     sess.headers['Accept-Language'] = 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6'
