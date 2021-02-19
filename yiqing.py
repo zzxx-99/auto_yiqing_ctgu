7a3249ffa4c479dd8a408a2ed0ef23cf0c8cfffa
zzxx-99 <71274663+zzxx-99@users.noreply.github.com>
2021-02-19 13:48:50 +0800

    Update yiqing.py

--- yiqing.py
+++ yiqing.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from requests import Session
+from bs4 import BeautifulSoup
 from urllib3 import encode_multipart_formdata
 from datetime import datetime,timedelta
 from re import findall
@@ -12,7 +13,13 @@ tmp = datetime.today()+timedelta(hours=8)
 today = tmp.strftime("%Y-%m-%d")
 log = []
 
-def report(usr,pas):
+
+def sentMsg(msg, key):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    api_url = "https://sc.ftqq.com/"key.send?text='.urlencode('msg')"
+    return requests.post(api_url, headers=headers, timeout=None).content
+
+def report(usr,pas,key,proxy):
     sess = Session()
     sess.headers['Content-Type'] = 'multipart/form-data; boundary=\
         ----WebKitFormBoundary5lPtCfVeRqiu7n6h'
@@ -25,7 +32,13 @@ def report(usr,pas):
     sess.headers['Accept-Language'] = 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6'
     sess.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;\
         q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-    r=sess.get('http://yiqing.ctgu.edu.cn/wx/index/main.do?currSchool=ctgu&CURRENT_YEAR=2019&showWjdc=false&studentShowWjdc=false')
+    r=sess.get('http://yiqing.ctgu.edu.cn/wx/index/login.do?currSchool=ctgu&CURRENT_YEAR=2019&showWjdc=false&studentShowWjdc=false')
+    
+    proxies = {
+    'http': proxy,
+    'https': proxy
+    }
+     yiqingSession = requests.session()
     
     data=encode_multipart_formdata({'username':usr,'password':pas},
                                    '----WebKitFormBoundary5lPtCfVeRqiu7n6h')
@@ -34,6 +47,13 @@ def report(usr,pas):
     if(login.text!='success'):
         log.append([[usr,pas],usr])
         #return 0
+
+    try:
+        responseRes = yiqingSession.post(
+            logUrl, data=postData,headers =header,timeout=None, proxies=proxies)
+    except:
+        sentMsg('网页无响应/请更换代理', key)
+        
     r=sess.post('http://yiqing.ctgu.edu.cn/wx/health/studentHis.do')
     his=eval(r.text.replace('null','None'))
     r=sess.get('http://yiqing.ctgu.edu.cn/wx/health/toApply.do')
@@ -64,6 +84,11 @@ def report(usr,pas):
         'sffx':'',
         'qt':'',
     }
+    
+     # 获取必要信息填入表单
+    soup = BeautifulSoup(responseRes.text, "html.parser")
+    getFormlist = soup.find_all('input')[0:15]
+    
     if his[0]['sbrq']!=today:
         for key in apply:
             apply[key] = his[0][key] if his[0][key]!=None else ''
@@ -74,9 +99,14 @@ def report(usr,pas):
     else:
         log.append([[usr,pas],strftime("%Y-%m-%d %H:%M:%S",localtime(his[0]['scrq']/1000))+' 已上报  '+his[0]['xm']])
     sess.close()
+     responseRes = yiqingSession.post(
+         postFormurl, data=postData,headers =header, verify=False, timeout=None, proxies=proxies)
+     print(responseRes.text)
+     sentMsg(username + ':'+ responseRes.text, key)
 stime = time()
 
-for usr,pas in parm:
+for usr,pas,key,proxy in parm:
+    sentOne(username, password, key,proxy)
     #start_new_thread(report,(usr,pas,))
     report(usr,pas)
     #print(log[-1][-1])
